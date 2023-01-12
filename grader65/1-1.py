4021e4cfb8794212e6f42de7ea22484d34702be6
paopeaw <paopeaw8246@gmail.com>
2023-01-12 15:39:28 +0700

    Update 1-1.py

--- grader65/1-1.py
+++ grader65/1-1.py
@@ -1,1 +1,9 @@
-# Finished bitch
+import math
+n = int(input())
+p1 = math.sqrt(2*math.pi)
+p2 = pow(n,n+.5)
+
+b = p1*p2* math.pow(math.e, (-n+(1/(12*n+1))))
+t = p1*p2* math.pow(math.e, (-n+(1/(12*n))))
+
+print("{0} {1}".format(b,t)) 
