9cbfcac77450237e620127b0709f357114266ec1
paopeaw <paopeaw8246@gmail.com>
2023-01-22 12:29:21 +0700

    finish 2-8.py

--- grader65/2-8.py
+++ grader65/2-8.py
@@ -8,14 +8,19 @@ a = n[1] + n[2]
 na = n[1]
 t = int(a) - int(na)
 b = "9"* len(n[2]) + "0" * (len(na)-1)
+b = 10 ** (len(a)-1) - 10 ** (len(na)-1)
 
 t += int(f) * int(b)
 
+gcd = math.gcd(t,b)
 
-# print(t,b)
+t //= gcd
+b //= gcd
+print("{0} / {1}".format(t,b))
 
-gcd = math.gcd(t,int(b))
-print("{0} / {1}".format( int(t/gcd), int(int(b)/gcd)))
+
+
+
 
 
 
