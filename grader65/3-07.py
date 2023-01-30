e63f69d13fb6ba72dfae56ee768b9338faa86b6c
paopeaw <paopeaw8246@gmail.com>
2023-01-30 13:53:38 +0700

    Finish grader

--- grader65/3-07.py
+++ grader65/3-07.py
@@ -3,6 +3,18 @@ n = int(input())
 if n < 1000:
     print(n)
 elif n < 1000000:
-    print(str(round(n/1000, 1)))
+    if n >= 10000 or n % 1000 == 0:
+        print(str(round(n/1000)) + "K")
+    else:
+        print(str(round(n/1000, 1)) + "K")
+elif n < 1000000000:
+    if n >= 10000000 or n % 1000000 == 0:
+        print(str(round(n/1000000)) + "M")
+    else:
+        print(str(round(n/1000000, 1)) + "M")
+elif n >= 1000000000:
+    if n >= 10000000000 or n %1000000000 == 0:
+        print(str(round(n/1000000000)) + "B")
+    else:
+        print(str(round(n/1000000000,1)) + "B")
 
-elif n > 1000000000:
